ed33ee652f93e8a875a9244fb6a554db83f29582
Dom Charlesworth <dgc336@gmail.com>
2017-08-08 14:18:11 +0100

    📦 Point main at `src/index` so that this module can be imported

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,19 @@
 const suggest = require('../src')
 
 // Need to pre process the inputs to cast them into their actual types?
-// Eslint can fuck off
-// eslint-disable-next-line
-let [ output, ...inputs ] = process.argv.slice(2).reverse().map(eval)
+let [ output, ...inputs ] = process.argv
+    .slice(2)
+    .reverse()
+    .map(s => {
+      try {
+        // Eval is used here to easily cast data types, eslint can shutup
+        // eslint-disable-next-line
+        return eval(s)
+      } catch (e) {
+        return s
+      }
+    })
+
 inputs = inputs.reverse()
 
 suggest(inputs, output)
