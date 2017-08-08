6e0e68c5d21af071d418bdcda8981a05633c5c4e
Dom Charlesworth <dgc336@gmail.com>
2017-08-08 16:30:22 +0100

    🦋 Fix the joining of params

--- src/print-report.js
+++ src/print-report.js
@@ -5,7 +5,7 @@ module.exports = async (inputs, output, func) => {
   const { doc, params, returns, category } = await docString(func)
   const outputString = printInput([output])
   const inputString = printInput(inputs)
-  const paramString = params.map(({ type, doc }, i) => ` param ${i + 1}: {${type}} ${doc}`)
+  const paramString = params.map(({ type, doc }, i) => ` param ${i + 1}: {${type}} ${doc}`).join('\n')
 
   console.log(`
 ${func}  [type:${category}] : R.${func}(${inputString}) → ${outputString}
