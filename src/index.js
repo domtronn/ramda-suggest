8524fd0c9f35199a81655fdd7607903246c36412
Dom Charlesworth <dgc336@gmail.com>
2017-08-09 09:42:05 +0100

    🎧 Allow you to test things where the output is a function

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ const deepEqual = require('deep-equal')
 module.exports = async (inputs, output) => {
   const results = Object
         .entries(R) // Get list of all available Ramda functions
-        .filter(([name, f]) => f.length === inputs.length) // Match signature length with input length
+        .filter(([name, f]) => /compose/.test(name) || f.length === inputs.length) // Match signature length with input length
         .filter(([name]) => name !== 'unfold')
         .filter(([name, f]) => { // Test function with input and compare to desired output
           try {
-            return deepEqual(f.apply({}, inputs), output)
+            let result = f.apply({}, inputs)
+
+            return (typeof result === 'function' && typeof output === 'function')
+              ? deepEqual(result(), output())
+              : deepEqual(result, output)
           } catch (ex) { return false }
         })
 
