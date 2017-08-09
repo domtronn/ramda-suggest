6a9e4817d705da50c791b8b91c7066956392fe69
Dom Charlesworth <dgc336@gmail.com>
2017-08-09 11:52:43 +0100

    🐏 Refactor to use ramda functions as it's a little tidier

--- src/print-input-string.js
+++ src/print-input-string.js
@@ -1,5 +1,12 @@
-module.exports = (inputs) => inputs.map(input => {
-  return typeof input === 'function'
-    ? input.toString()
-    : JSON.stringify(input)
-}).join(', ')
+const { cond, is, compose, map, join, T, toString } = require('ramda')
+const { fromCharCode: char } = String
+
+// Text formatters
+const formatArgs = (arg, i) => `${char(i + 97)}: ${arg}`
+const formatPred = cond([
+  [is(Function), toString],
+  [is(Object), ({ args, ret }) => `(${args.map(formatArgs)}) => ${ret}`],
+  [T, JSON.stringify]
+])
+
+module.exports = compose(join(', '), map((input) => formatPred(input)))
