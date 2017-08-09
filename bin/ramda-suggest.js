6c6de5565c27841f8ff1c125c6f6a5dfc2873d18
Dom Charlesworth <dgc336@gmail.com>
2017-08-09 09:41:07 +0100

    🎻 Tidy up the parsing of arguments into a function

--- bin/ramda-suggest.js
+++ bin/ramda-suggest.js
@@ -1,22 +1,19 @@
 #! /usr/bin/env node
 const suggest = require('../src')
+const parseArg = (arg) => {
+  try {
+    return JSON.parse(arg)
+  } catch (e) {
+    try {
+      // Eval is used here to easily cast data types, eslint can shutup
+      // eslint-disable-next-line
+      return eval(arg)
+    } catch (e) { return arg.toString() }
+  }
+}
 
 // Need to pre process the inputs to cast them into their actual types?
-let [ output, ...inputs ] = process.argv
-    .slice(2)
-    .reverse()
-    .map(s => {
-      try {
-        // Eval is used here to easily cast data types, eslint can shutup
-        // eslint-disable-next-line
-        return eval(s)
-      } catch (e) {
-        try {
-          return JSON.parse(s)
-        } catch (e) { return s }
-      }
-    })
-
+let [ output, ...inputs ] = process.argv.slice(2).reverse().map(parseArg)
 inputs = inputs.reverse()
 
 suggest(inputs, output)
