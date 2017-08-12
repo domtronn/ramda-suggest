d1c88a351b4aa0237945cd501cfb2f6c9a95841b
Dom Charlesworth <dgc336@gmail.com>
2017-08-12 17:27:21 +0100

    [fix #1] Log a usage message if no output/inputs given

--- bin/ramda-suggest.js
+++ bin/ramda-suggest.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+const { grey, bold } = require('chalk')
 const suggest = require('../src')
 const parseArg = (arg) => {
   try {
@@ -10,6 +11,16 @@ const parseArg = (arg) => {
       return eval(arg)
     } catch (e) { return arg.toString() }
   }
+}
+
+if (process.argv.length < 3) {
+  console.log(`
+${bold('ramda-suggest')}: Missing arguments
+
+  Try: ${grey('ramda-suggest input1 input2 output2')}
+       ${grey('ramda-suggest true')}
+`)
+  process.exit(0)
 }
 
 // Need to pre process the inputs to cast them into their actual types?
